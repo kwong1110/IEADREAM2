47739711d88fabb97b6ff058b5dbaaf6d90f43f8
YSG <YSG@DESKTOP-TCC74HV>
2020-03-11 02:37:58 +0900

    questionService에러난거 다시 커밋!

--- src/board/model/service/QuestionService.java
+++ src/board/model/service/QuestionService.java
@@ -154,27 +154,6 @@ public class QuestionService {
 		return list;
 	}
 
-	//  페이징 - user 리스트 뷰
-	public ArrayList<Board> selectQBList(int currentPage, String userNo) {
-		Connection conn = getConnection();
-		QuestionDAO dao = new QuestionDAO();
-		
-		ArrayList<Board> list = new QuestionDAO().selectQBList(conn, currentPage,userNo); 
-		System.out.println("b : " + list);
-		close(conn);
-		return list;
-	}
-	
-	public ArrayList<Reply> selectQRList(int currentPage, String userNo) {
-		Connection conn = getConnection();
-		QuestionDAO dao = new QuestionDAO();
-		
-		ArrayList<Reply> r = dao.selectQRList(conn,currentPage, userNo);
-		System.out.println("r " + r);
-		close(conn);
-		  return r;
-		
-	}
 
 	// 페이징 - 관리자 리스트 뷰
 	 public int MgetListCount() { 
